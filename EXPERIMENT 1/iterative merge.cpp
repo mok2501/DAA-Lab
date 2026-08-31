05001412db2dad008333586139bf6f9ff8221a49
mok2501 <ghawanamoksh@gmail.com>
2026-08-31 14:38:17 +0530

    Add files via upload

---
+++ EXPERIMENT 1/iterative merge.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int n)
+{
+    for (int width = 1; width < n; width *= 2)
+    {
+        for (int left = 0; left < n; left += 2 * width)
+        {
+            int mid = min(left + width - 1, n - 1);
+            int right = min(left + 2 * width - 1, n - 1);
+
+            if (mid < right)
+                merge(arr, left, mid, right);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    mergeSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
